9fb356e3bb4a4174c369f371bb8c12681257e769
Timothy Remst <11875728+tjaynl@users.noreply.github.com>
2023-07-04 21:30:18 +0200

    fixed static images

--- _config.ts
+++ _config.ts
@@ -7,7 +7,6 @@ import netlify_cms from "lume/plugins/netlify_cms.ts";
 import postcss from "lume/plugins/postcss.ts";
 import sass from "lume/plugins/sass.ts";
 import svgo from "lume/plugins/svgo.ts";
-import esbuild from "lume/plugins/esbuild.ts";
 
 const site = lume({
     location: new URL("https://rosita.nl"),
@@ -17,25 +16,10 @@ const site = lume({
 site.use(attributes())
     .use(sass())
     .use(postcss())
-    .use(esbuild({
-        extensions: [".ts", ".js"],
-        options: {
-            bundle: true,
-            format: "esm",
-            minify: true,
-            keepNames: true,
-            platform: "browser",
-            target: "esnext",
-            incremental: true,
-            treeShaking: true,
-        },
-    }))
     .use(metas())
     .use(imagick())
     .use(inline())
     .use(svgo())
     .use(netlify_cms());
-
-site.copy("static", ".");
 
 export default site;
