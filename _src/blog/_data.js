6f219108857bbbf36214b4fc8a6f28a464a2a5e1
Timothy Remst <11875728+tjaynl@users.noreply.github.com>
2023-07-10 22:33:04 +0200

    fixed images paths

--- _src/blog/_data.js
+++ _src/blog/_data.js
@@ -2,7 +2,7 @@ export const type = "post";
 export const layout = "layouts/blog-post.njk";
 export const templateEngine = "njk,md";
 export const bodyClass = "post";
-export const summary = "=summary";
+export const image = "=image";
 export const areaDescription = "Radio Rosita Blog";
 
 export function url(page) {
