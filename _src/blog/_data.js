628da3bf31787b28b819de43bcc741fec9666707
Timothy Remst <11875728+tjaynl@users.noreply.github.com>
2023-07-11 01:54:37 +0200

    Update _data.js

--- _src/blog/_data.js
+++ _src/blog/_data.js
@@ -2,7 +2,6 @@ export const type = "post";
 export const layout = "layouts/blog-post.njk";
 export const templateEngine = "njk,md";
 export const bodyClass = "post";
-export const image = "=image";
 export const areaDescription = "Radio Rosita Blog";
 
 export function url(page) {
